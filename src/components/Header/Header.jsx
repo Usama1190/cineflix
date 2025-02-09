01f74086a19d06bb5df922fa86d114b3dd6ea7a0
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2025-02-09 23:14:40 +0500

    Now its time to deploy my project on vercel let get started on feb 09, 25

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,38 @@
 import { NavLink } from "react-router-dom";
 import Button from "../common/Button/Button";
 import styles from "./Header.module.css";
+import { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const clickHandle = () => {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  };
   return (
     <div className={styles.hw}>
       <div className={styles.hfw}>
         <h1>Cineflex</h1>
-        <ul>
-          <li>
-            <NavLink to={"/"}>Home</NavLink>
-          </li>
-          <li>
-            <NavLink to={"/movies"}>Movies</NavLink>
-          </li>
-          <li>
-            <NavLink to={"/upcoming-posters"}>Upcoming Posters</NavLink>
-          </li>
-          <li>
-            <Button btnText={"Login"} btnLink={"/login"} btnLayout={'sf'} />
-          </li>
-        </ul>
+        <div className={isOpen ? `${styles.lw} ${styles.dl}` : `${styles.lw} ${styles.hl}`} onClick={clickHandle}>
+          <ul>
+            <li>
+              <NavLink to={"/"}>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/movies"}>Movies</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/upcoming-posters"}>Upcoming Posters</NavLink>
+            </li>
+            <li>
+              <Button btnText={"Login"} btnLink={"/login"} btnLayout={"sf"} />
+            </li>
+          </ul>
+        </div>
+        <div className={styles.mb} onClick={clickHandle}>
+          <i className="fa-solid fa-bars"></i>
+        </div>
       </div>
     </div>
   );
