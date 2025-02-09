8e955ebde66bae3187a10c3fba30b1b47de172b3
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2025-02-09 15:00:38 +0500

    Amost done this project working on layouting on feb 09, 25

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,8 +1,17 @@
+import Button from "../common/Button/Button"
+import styles from './Error.module.css'
 
 const Error = () => {
   return (
-    <div>
-      Error - 404 - Not Found!
+    <div className={styles.ew}>
+      <div className={styles.efw}>
+        <h1>404</h1>
+      </div>
+      <div className={styles.esw}>
+        <h2>Sorry, Page Not Found</h2>
+        <p>The page you requested could not found</p>
+        <Button btnLayout={'sf'} btnLink={'/'} btnText={'Go Back Home'} />
+      </div>
     </div>
   )
 }
--- src/components/common/MovieCard/MovieCard.jsx
+++ src/components/common/MovieCard/MovieCard.jsx
@@ -2,15 +2,15 @@ import { NavLink } from "react-router-dom";
 import styles from "./MovieCard.module.css";
 
 const MovieCard = ({ data }) => {
-  const { id, title, year, image_url, img_alt } = data;
+  const { id, rating, year, image_url, img_alt } = data;
   return (
     <NavLink to={`/movies/${id}`}>
       <div className={styles.mcw}>
         <div className={styles.mci}>
-        <img src={image_url} alt={img_alt} />
+          <img src={image_url} alt={img_alt} />
         </div>
-        <h1>{title}</h1>
         <p>Realeased: {year}</p>
+        <p>Rating : {rating}</p>
       </div>
     </NavLink>
   );
